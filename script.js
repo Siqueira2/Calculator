b6046471d8b5de7bcf3d6b5fcac145d5612a3f11
Guilherme Siqueira <107515511+Siqueira2@users.noreply.github.com>
2022-12-26 16:20:39 -0300

    update commit

--- script.js
+++ script.js
@@ -7,14 +7,15 @@ let firstnum,
     op = ""
 
 keys.addEventListener("click", (e)=>{
-
     const key = e.target.innerText
     
     if(Number(key) >= 0 || key === "."){
         insertNumber(key)
+        previousOperation.innerHTML += key
     }
 
     if(key === "*" || key === "/" || key === "+" || key === "-"){
+        previousOperation.innerHTML += key
         display.textContent = 0
         op = key
         operator = true
@@ -28,6 +29,22 @@ keys.addEventListener("click", (e)=>{
         clear()
     }
 })
+
+/*document.addEventListener("keydown", (event)=>{
+    if(Number(event.key) >= 0 || event.key === "."){
+        insertNumber(key)
+    }
+
+    if(event.key === "*" || event.key === "/" || event.key === "+" || event.key === "-"){
+        display.textContent = 0
+        op = event.key
+        operator = true
+    }
+
+    if(event.key === "="){
+        calculate()
+    }
+})*/
 
 function insertNumber(value){
     if(display.textContent === "0"){
@@ -48,6 +65,7 @@ function clear(){
     firstnum = 0
     lastnum = 0
     operator = false
+    previousOperation.innerHTML = ""
     display.textContent = "0"
     console.log(`firstnum: ${firstnum}`)
     console.log(`lastnum: ${lastnum}`)
